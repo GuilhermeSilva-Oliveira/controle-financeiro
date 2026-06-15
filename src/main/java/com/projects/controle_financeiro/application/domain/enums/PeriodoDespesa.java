8bb34b757829cb6ac21f623f38296d6813110694
GuilhermeSilva-Oliveira <guilhermesilvaoliveira2128@gmail.com>
2026-06-15 14:37:50 -0300

    refactor: refatoração de função de pagamento de despesa

--- src/main/java/com/projects/controle_financeiro/application/domain/enums/PeriodoDespesa.java
+++ src/main/java/com/projects/controle_financeiro/application/domain/enums/PeriodoDespesa.java
@@ -8,4 +8,10 @@ public enum PeriodoDespesa {
     private final String periodo;
     PeriodoDespesa(String periodo){this.periodo = periodo;}
     public String getPeriodo() {return periodo;}
+    public static PeriodoDespesa fromPeriodo(String periodo){
+        for (PeriodoDespesa p : values()){
+            if (p.getPeriodo().equals(periodo)){return p;}
+        }
+        throw new IllegalArgumentException("Período Inválido");
+    }
 }
